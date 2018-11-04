b215a987c7d63b48a0bc4af72aeeda9675b2f6d1
Győri Sándor <alexaegis@gmail.com>
2018-11-04 19:52:03 +0100

    added read option

--- sub/sub1.c
+++ sub/sub1.c
@@ -11,20 +11,11 @@ struct order
 {
 	int id;
 	char name[255];
+	char email[255];
+	char phone[255];
+	char perf[255];
 	time_t time;
 };
-
-// array pointer and the size of that
-struct arrs
-{
-	int pt;
-	int s;
-};
-
-int list(struct order *data, int size)
-{
-	return 0;
-}
 
 struct order interpret_line(char *line)
 {
@@ -34,12 +25,6 @@ struct order interpret_line(char *line)
 	int col = 0;
 	while (pt != NULL)
 	{
-		//printf("column %d\n", col);
-		/*
-		*/
-
-		//if (found == 1)
-		//{
 		switch (col)
 		{
 		case 0:
@@ -52,9 +37,6 @@ struct order interpret_line(char *line)
 			result.time = time(NULL);
 			break;
 		}
-		//}
-		//printf("%d.: %s\n", col, pt);
-
 		pt = strtok(NULL, ",");
 		col = col + 1;
 	}
@@ -64,37 +46,23 @@ struct order interpret_line(char *line)
 
 struct order *filter(struct order *filter_obj, int *size, int here)
 {
-	if (filter_obj == NULL)
-	{
-		printf("You ahve no filter, list all");
-	}
-	else
-	{
-		printf("Your filter: %d, %s\n", filter_obj->id, filter_obj->name);
-	}
-
 	FILE *file = fopen("data.txt", "r");
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-
+	//printf("Filter. size: %d, here: %d", *size, here);
 	struct order *res = NULL;
 
 	if (here > 0)
 	{
+		//printf("MALLOCCED");
 		res = malloc(here * sizeof(struct order));
-
-		printf("MALLOC HAPPENED!!!!here:  %d size: %d\n", here, *size);
 	}
-
+	//printf("1");
 	*size = here;
-	printf("size ASSIGNED!!!!here:  %d size: %d\n", here, *size);
-
 	int found = 0;
 	while ((read = getline(&line, &len, file)) != -1)
 	{
-		//printf("Retrieved line of length %zu :\n", read);
-		printf("Line: %s", line);
 		struct order a = interpret_line(line);
 		int match = 1;
 		if (filter_obj != NULL)
@@ -104,13 +72,11 @@ struct order *filter(struct order *filter_obj, int *size, int here)
 			if (filter_obj->id >= 0 && filter_obj->id == a.id)
 			{
 				matches_by_id = 0;
-				printf("no match by id\n");
 			}
 			int matches_by_name = 1;
 			if (filter_obj->name[0] != '\0' && strcmp(filter_obj->name, a.name) != 0)
 			{
 				matches_by_name = 0;
-				printf("no match by name, compared: %s, and %s \n", filter_obj->name, a.name);
 			}
 
 			if (matches_by_id != 1 || matches_by_name != 1)
@@ -120,11 +86,8 @@ struct order *filter(struct order *filter_obj, int *size, int here)
 		}
 		if (match == 1)
 		{
-			printf("Found order %d\n", found);
-
 			if (res != NULL)
 			{
-				printf("Found order WRITE TO ARRAY: %d\n", a.id);
 				res[found] = a;
 			}
 
@@ -133,14 +96,14 @@ struct order *filter(struct order *filter_obj, int *size, int here)
 	}
 	fclose(file);
 
-	if (res == NULL) // I know the size, go for filter
+	if (res == NULL && found > 0) // I know the size, go for filter
 	{
-		printf("I know the size, go for filter\n");
+		//printf("I know the size, go for filter, size: %d, found: %d", size, found);
 		return filter(filter_obj, size, found);
 	}
 	else // I know the result, go for return
 	{
-		printf("I know the result, go for return\n");
+		//printf("I know the result, go for return");
 		return res;
 	}
 }
@@ -176,12 +139,10 @@ int update(struct order *filter_obj, struct order *update_obj)
 		fgets(str, 255, fptr1);
 		if (!feof(fptr1))
 		{
-			printf("LINE %s\n", str);
 			char line[255];
 			strcpy(line, str);
 
 			struct order a = interpret_line(str);
-			printf("LINE AGAIN %s\n", line);
 			int match = 1;
 			if (filter_obj != NULL)
 			{
@@ -190,13 +151,11 @@ int update(struct order *filter_obj, struct order *update_obj)
 				if (filter_obj->id >= 0 && filter_obj->id == a.id)
 				{
 					matches_by_id = 0;
-					printf("no match by id\n");
 				}
 				int matches_by_name = 1;
 				if (filter_obj->name[0] != '\0' && strcmp(filter_obj->name, a.name) != 0)
 				{
 					matches_by_name = 0;
-					printf("no match by name, compared: %s, and %s \n", filter_obj->name, a.name);
 				}
 
 				if (matches_by_id != 1 || matches_by_name != 1)
@@ -208,7 +167,6 @@ int update(struct order *filter_obj, struct order *update_obj)
 			ctr++;
 			if (match != 1)
 			{
-				printf("MAYA HEEE %s", line);
 				fprintf(fptr2, "%s", line);
 			}
 			else
@@ -242,26 +200,42 @@ int delete (struct order *filter_obj)
 	return update(filter_obj, NULL);
 }
 
+void print_order(struct order *o)
+{
+	printf("{id: %d, name: %s, email: %s, phone: %s, perf: %s, time: %s}\n", o->id, &o->name, &o->email, &o->phone, &o->perf, ctime(&o->time));
+}
+
+struct order *read_order()
+{
+	struct order *result = malloc(sizeof(struct order));
+	result->id = -1;
+	printf("name:");
+	scanf("%s", result->name);
+	printf("email:");
+	scanf("%s", result->email);
+	printf("phone:");
+	scanf("%s", result->phone);
+	printf("perf:");
+	scanf("%s", result->perf);
+	result->time = time(NULL);
+	return result;
+}
+
 int main()
 {
-	struct order data[100];
-
-	struct order example;
-	example.id = 0;
-	strcpy(example.name, "NAAAAME");
-	example.time = time(NULL);
 
 	// CREATE - Write to file
 	// create(&example);
 
 	// READ - With filter
 	/*
-	struct order filter_obj;
-	filter_obj.id = -1;
-	strcpy(filter_obj.name, "numa");
+	struct order *filter_obj = read_order();
+	//filter_obj.id = -1;
+	//strcpy(filter_obj.name, "numa");
+
 	int c = -1;
 	int *count = &c;
-	struct order *result = filter(&filter_obj, count, -1);
+	struct order *result = filter(filter_obj, count, -1);
 	printf("RETURNED RES SIZE FOR FILTER: %d\n", *count);
 	int i = 0;
 	while (i < *count)
@@ -274,30 +248,16 @@ int main()
 */
 	// READ - Without filter (read all)
 	/*
-	printf("READ - Without filter (read all)\n");
-
-	int _c = -1;
-	int *_count = &_c;
-	printf("READ \n");
-	struct order *_result = filter(NULL, _count, -1);
-	printf("RETURNED RES SIZE FOR FILTER: %d\n", *_count);
-	int _i = 0;
-	while (_i < *_count)
-	{
-		printf("READ THE FILTERED RESULT %d\n", _i);
-		struct order *_r = &_result[_i];
-		printf("RETURNED RES: id: %d name: %s\n", _r->id, _r->name);
-		_i++;
-	};
+	
 */
 
 	// DELETE
-
+	/*
 	struct order filter_obj;
 	filter_obj.id = -1;
 	strcpy(filter_obj.name, "numa");
 	delete (&filter_obj);
-
+*/
 	// UPDATE
 	/*
 	struct order filter_obj;
@@ -310,34 +270,51 @@ int main()
 
 	update(&filter_obj, &update_obj);
 */
-	/*
-	printf("size of: %d", LENGTH(data));
-	printf("current %d", c);
-	append(data, &example, &c, LENGTH(data));
-	printf("current %d", c);
-	read(&example);
-	read(&data[0]);
 
 	char menu_option;
 	do
 	{
-		printf("0. List\n");
+		printf("0. List (All)\n");
 		printf("1. Create\n");
-		printf("2. Read\n");
-		printf("3. Update\n");
-		printf("4. Delete\n");
+		printf("2. Read (Filter)\n");
+		printf("3. Update (Filter) (Replacement)\n");
+		printf("4. Delete (Filter)\n");
 		printf("q. Exit\n");
 		scanf(" %c", &menu_option);
 		switch (menu_option)
 		{
 		case '0':
-			printf("0\n");
+			printf("0 - LIST MODE\n");
+			int _c = -1;
+			int *_count = &_c;
+			struct order *_result = filter(NULL, _count, -1);
+			int _i = 0;
+			while (_i < *_count)
+			{
+				struct order *_r = &_result[_i];
+				print_order(_r);
+				_i++;
+			};
 			break;
 		case '1':
-			printf("1\n");
+			printf("1 - CREATE MODE\n");
 			break;
 		case '2':
-			printf("2\n");
+			printf("2 - READ MODE\n");
+			struct order *_read_filter = read_order();
+			// print_order(_read_filter);
+
+			int _read_c = -1;
+			int *_read_count = &_read_c;
+			struct order *_read_result = filter(_read_filter, _read_count, -1);
+			int _read_i = 0;
+			while (_read_i < *_read_count)
+			{
+				struct order *_read_r = &_read_result[_i];
+				print_order(_read_r);
+				_read_i++;
+			};
+
 			break;
 		case 'q':
 			printf("Quit\n");
@@ -345,6 +322,6 @@ int main()
 		}
 		printf("\nYour choice is: %c\n", menu_option);
 	} while (menu_option != 'q');
-*/
+
 	return 0;
 }
