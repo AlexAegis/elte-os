c8a7c7ac8d213e64ae0c802f1000c602d852d24c
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-11-05 13:46:36 +0100

    format

--- gy3/nevcso.c
+++ gy3/nevcso.c
@@ -6,42 +6,42 @@
 #include <fcntl.h>
 #include <errno.h> // for errno, the number of last error
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int pid,fd;
-    printf("Fifo start!\n");
-    int fid=mkfifo("fifo.ftc", S_IRUSR|S_IWUSR ); // creating named pipe file
-    // S_IWGRP, S_IROTH (other jog), file permission mode
-    // the file name: fifo.ftc
-    // the real fifo.ftc permission is: mode & ~umask 
-    if (fid==-1)
-    {
-	printf("Error number: %i",errno);
-	exit(EXIT_FAILURE);
-    }
-    printf("Mkfifo vege, fork indul!\n");
-    pid = fork();
-    
-    if(pid>0)   //parent 
+	int pid, fd;
+	printf("Fifo start!\n");
+	int fid = mkfifo("fifo.ftc", S_IRUSR | S_IWUSR); // creating named pipe file
+	// S_IWGRP, S_IROTH (other jog), file permission mode
+	// the file name: fifo.ftc
+	// the real fifo.ftc permission is: mode & ~umask
+	if (fid == -1)
 	{
-	char s[1024]="Semmi";		
-	printf("Csonyitas eredmenye szuloben: %d!\n",fid);
-	fd=open("fifo.ftc",O_RDONLY);
-	read(fd,s,sizeof(s));
-	printf("Ezt olvastam a csobol: %s \n",s);
-	close(fd);
-	// remove fifo.ftc
-	unlink("fifo.ftc");
-        }
+		printf("Error number: %i", errno);
+		exit(EXIT_FAILURE);
+	}
+	printf("Mkfifo vege, fork indul!\n");
+	pid = fork();
+
+	if (pid > 0) //parent
+	{
+		char s[1024] = "Semmi";
+		printf("Csonyitas eredmenye szuloben: %d!\n", fid);
+		fd = open("fifo.ftc", O_RDONLY);
+		read(fd, s, sizeof(s));
+		printf("Ezt olvastam a csobol: %s \n", s);
+		close(fd);
+		// remove fifo.ftc
+		unlink("fifo.ftc");
+	}
 	else // child
 	{
-	printf("Gyerek vagyok, irok a csobe!\n");
-	printf("Csonyitas eredmenye: %d!\n",fid);
-        fd=open("fifo.ftc",O_WRONLY);
-        write(fd,"Hajra Fradi!\n",12);
-        close(fd);
-	printf("Gyerek vagyok, beirtam, vegeztem!\n");
-    	}	
-    
-    return 0; 
+		printf("Gyerek vagyok, irok a csobe!\n");
+		printf("Csonyitas eredmenye: %d!\n", fid);
+		fd = open("fifo.ftc", O_WRONLY);
+		write(fd, "Hajra Fradi!\n", 12);
+		close(fd);
+		printf("Gyerek vagyok, beirtam, vegeztem!\n");
+	}
+
+	return 0;
 }
